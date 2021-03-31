36460b1073e8315d0a771612d21b7f2679496cc1
zment <jani.karkkainen@gmail.com>
2021-03-31 18:47:34 +0300

    First commit

---
+++ MasterThrind/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿
+namespace MasterThrind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewItem1 = new System.Windows.Forms.ListView();
+            this.listViewItem2 = new System.Windows.Forms.ListView();
+            this.listViewItem3 = new System.Windows.Forms.ListView();
+            this.listViewResult1 = new System.Windows.Forms.ListView();
+            this.listViewResult2 = new System.Windows.Forms.ListView();
+            this.listViewResult3 = new System.Windows.Forms.ListView();
+            this.symbol1 = new System.Windows.Forms.Button();
+            this.symbol2 = new System.Windows.Forms.Button();
+            this.symbol3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.symbol4 = new System.Windows.Forms.Button();
+            this.symbol5 = new System.Windows.Forms.Button();
+            this.symbol6 = new System.Windows.Forms.Button();
+            this.symbol7 = new System.Windows.Forms.Button();
+            this.symbol8 = new System.Windows.Forms.Button();
+            this.symbol9 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listViewItem1
+            // 
+            this.listViewItem1.AutoArrange = false;
+            this.listViewItem1.HideSelection = false;
+            this.listViewItem1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listViewItem1.Location = new System.Drawing.Point(98, 42);
+            this.listViewItem1.MultiSelect = false;
+            this.listViewItem1.Name = "listViewItem1";
+            this.listViewItem1.Size = new System.Drawing.Size(24, 396);
+            this.listViewItem1.TabIndex = 0;
+            this.listViewItem1.UseCompatibleStateImageBehavior = false;
+            this.listViewItem1.View = System.Windows.Forms.View.List;
+            // 
+            // listViewItem2
+            // 
+            this.listViewItem2.HideSelection = false;
+            this.listViewItem2.Location = new System.Drawing.Point(120, 42);
+            this.listViewItem2.Name = "listViewItem2";
+            this.listViewItem2.Size = new System.Drawing.Size(24, 396);
+            this.listViewItem2.TabIndex = 1;
+            this.listViewItem2.UseCompatibleStateImageBehavior = false;
+            this.listViewItem2.View = System.Windows.Forms.View.List;
+            // 
+            // listViewItem3
+            // 
+            this.listViewItem3.HideSelection = false;
+            this.listViewItem3.Location = new System.Drawing.Point(142, 42);
+            this.listViewItem3.Name = "listViewItem3";
+            this.listViewItem3.Size = new System.Drawing.Size(24, 396);
+            this.listViewItem3.TabIndex = 2;
+            this.listViewItem3.UseCompatibleStateImageBehavior = false;
+            this.listViewItem3.View = System.Windows.Forms.View.List;
+            // 
+            // listViewResult1
+            // 
+            this.listViewResult1.HideSelection = false;
+            this.listViewResult1.Location = new System.Drawing.Point(182, 42);
+            this.listViewResult1.Name = "listViewResult1";
+            this.listViewResult1.Size = new System.Drawing.Size(16, 396);
+            this.listViewResult1.TabIndex = 3;
+            this.listViewResult1.UseCompatibleStateImageBehavior = false;
+            this.listViewResult1.View = System.Windows.Forms.View.List;
+            // 
+            // listViewResult2
+            // 
+            this.listViewResult2.HideSelection = false;
+            this.listViewResult2.Location = new System.Drawing.Point(196, 42);
+            this.listViewResult2.Name = "listViewResult2";
+            this.listViewResult2.Size = new System.Drawing.Size(16, 396);
+            this.listViewResult2.TabIndex = 4;
+            this.listViewResult2.UseCompatibleStateImageBehavior = false;
+            this.listViewResult2.View = System.Windows.Forms.View.List;
+            // 
+            // listViewResult3
+            // 
+            this.listViewResult3.HideSelection = false;
+            this.listViewResult3.Location = new System.Drawing.Point(210, 42);
+            this.listViewResult3.Name = "listViewResult3";
+            this.listViewResult3.Size = new System.Drawing.Size(16, 396);
+            this.listViewResult3.TabIndex = 5;
+            this.listViewResult3.UseCompatibleStateImageBehavior = false;
+            this.listViewResult3.View = System.Windows.Forms.View.List;
+            // 
+            // symbol1
+            // 
+            this.symbol1.Location = new System.Drawing.Point(12, 42);
+            this.symbol1.Name = "symbol1";
+            this.symbol1.Size = new System.Drawing.Size(75, 23);
+            this.symbol1.TabIndex = 6;
+            this.symbol1.Text = "A1";
+            this.symbol1.UseVisualStyleBackColor = true;
+            this.symbol1.Click += new System.EventHandler(this.symbol1_Click);
+            // 
+            // symbol2
+            // 
+            this.symbol2.Location = new System.Drawing.Point(12, 71);
+            this.symbol2.Name = "symbol2";
+            this.symbol2.Size = new System.Drawing.Size(75, 23);
+            this.symbol2.TabIndex = 7;
+            this.symbol2.Text = "A2";
+            this.symbol2.UseVisualStyleBackColor = true;
+            this.symbol2.Click += new System.EventHandler(this.symbol2_Click);
+            // 
+            // symbol3
+            // 
+            this.symbol3.Location = new System.Drawing.Point(12, 100);
+            this.symbol3.Name = "symbol3";
+            this.symbol3.Size = new System.Drawing.Size(75, 23);
+            this.symbol3.TabIndex = 8;
+            this.symbol3.Text = "A3";
+            this.symbol3.UseVisualStyleBackColor = true;
+            this.symbol3.Click += new System.EventHandler(this.symbol3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(179, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Result";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(98, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Code";
+            // 
+            // symbol4
+            // 
+            this.symbol4.Location = new System.Drawing.Point(12, 129);
+            this.symbol4.Name = "symbol4";
+            this.symbol4.Size = new System.Drawing.Size(75, 23);
+            this.symbol4.TabIndex = 12;
+            this.symbol4.Text = "B1";
+            this.symbol4.UseVisualStyleBackColor = true;
+            this.symbol4.Click += new System.EventHandler(this.symbol4_Click);
+            // 
+            // symbol5
+            // 
+            this.symbol5.Location = new System.Drawing.Point(12, 158);
+            this.symbol5.Name = "symbol5";
+            this.symbol5.Size = new System.Drawing.Size(75, 23);
+            this.symbol5.TabIndex = 13;
+            this.symbol5.Text = "B2";
+            this.symbol5.UseVisualStyleBackColor = true;
+            this.symbol5.Click += new System.EventHandler(this.symbol5_Click);
+            // 
+            // symbol6
+            // 
+            this.symbol6.Location = new System.Drawing.Point(12, 187);
+            this.symbol6.Name = "symbol6";
+            this.symbol6.Size = new System.Drawing.Size(75, 23);
+            this.symbol6.TabIndex = 14;
+            this.symbol6.Text = "B3";
+            this.symbol6.UseVisualStyleBackColor = true;
+            this.symbol6.Click += new System.EventHandler(this.symbol6_Click);
+            // 
+            // symbol7
+            // 
+            this.symbol7.Location = new System.Drawing.Point(12, 216);
+            this.symbol7.Name = "symbol7";
+            this.symbol7.Size = new System.Drawing.Size(75, 23);
+            this.symbol7.TabIndex = 15;
+            this.symbol7.Text = "C1";
+            this.symbol7.UseVisualStyleBackColor = true;
+            this.symbol7.Click += new System.EventHandler(this.symbol7_Click);
+            // 
+            // symbol8
+            // 
+            this.symbol8.Location = new System.Drawing.Point(12, 245);
+            this.symbol8.Name = "symbol8";
+            this.symbol8.Size = new System.Drawing.Size(75, 23);
+            this.symbol8.TabIndex = 16;
+            this.symbol8.Text = "C2";
+            this.symbol8.UseVisualStyleBackColor = true;
+            this.symbol8.Click += new System.EventHandler(this.symbol8_Click);
+            // 
+            // symbol9
+            // 
+            this.symbol9.Location = new System.Drawing.Point(12, 274);
+            this.symbol9.Name = "symbol9";
+            this.symbol9.Size = new System.Drawing.Size(75, 23);
+            this.symbol9.TabIndex = 17;
+            this.symbol9.Text = "C3";
+            this.symbol9.UseVisualStyleBackColor = true;
+            this.symbol9.Click += new System.EventHandler(this.symbol9_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(233, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "X - Correct symbol and position";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(232, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "O - Correct symbol";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(232, 68);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "C - Correct character";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(232, 81);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "N - Correct number";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.symbol9);
+            this.Controls.Add(this.symbol8);
+            this.Controls.Add(this.symbol7);
+            this.Controls.Add(this.symbol6);
+            this.Controls.Add(this.symbol5);
+            this.Controls.Add(this.symbol4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.symbol3);
+            this.Controls.Add(this.symbol2);
+            this.Controls.Add(this.symbol1);
+            this.Controls.Add(this.listViewResult3);
+            this.Controls.Add(this.listViewResult2);
+            this.Controls.Add(this.listViewResult1);
+            this.Controls.Add(this.listViewItem3);
+            this.Controls.Add(this.listViewItem2);
+            this.Controls.Add(this.listViewItem1);
+            this.Name = "Form1";
+            this.Text = "MasterThrind";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewItem1;
+        private System.Windows.Forms.ListView listViewItem2;
+        private System.Windows.Forms.ListView listViewItem3;
+        private System.Windows.Forms.ListView listViewResult1;
+        private System.Windows.Forms.ListView listViewResult2;
+        private System.Windows.Forms.ListView listViewResult3;
+        private System.Windows.Forms.Button symbol1;
+        private System.Windows.Forms.Button symbol2;
+        private System.Windows.Forms.Button symbol3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button symbol4;
+        private System.Windows.Forms.Button symbol5;
+        private System.Windows.Forms.Button symbol6;
+        private System.Windows.Forms.Button symbol7;
+        private System.Windows.Forms.Button symbol8;
+        private System.Windows.Forms.Button symbol9;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ MasterThrind/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasterThrind
+{
+    public partial class Form1 : Form
+    {
+        private List<ListView> resultListViews;
+        private List<ListView> previousListViews = new List<ListView>();
+        private ListView currentListView;
+        private List<string> correctLine = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            currentListView = listViewItem1;
+            resultListViews = new List<ListView>() { listViewResult1, listViewResult2, listViewResult3 };
+
+            var random = new Random();
+            var possibleSymbols = new string[] { "A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3" }.ToList();
+            for (int i = 0; i < 3; i++)
+            {
+                var symbolIndex = random.Next(0, possibleSymbols.Count());
+                correctLine.Add(possibleSymbols[symbolIndex]);
+                possibleSymbols.RemoveAt(symbolIndex);
+            }
+        }
+
+        private void symbol1_Click(object sender, EventArgs e)
+        {
+            addSymbol("A1");
+        }
+
+        private void symbol2_Click(object sender, EventArgs e)
+        {
+            addSymbol("A2");
+        }
+
+        private void symbol3_Click(object sender, EventArgs e)
+        {
+            addSymbol("A3");
+        }
+
+        private void symbol4_Click(object sender, EventArgs e)
+        {
+            addSymbol("B1");
+        }
+
+        private void symbol5_Click(object sender, EventArgs e)
+        {
+            addSymbol("B2");
+        }
+
+        private void symbol6_Click(object sender, EventArgs e)
+        {
+            addSymbol("B3");
+        }
+
+        private void symbol7_Click(object sender, EventArgs e)
+        {
+            addSymbol("C1");
+        }
+
+        private void symbol8_Click(object sender, EventArgs e)
+        {
+            addSymbol("C2");
+        }
+
+        private void symbol9_Click(object sender, EventArgs e)
+        {
+            addSymbol("C3");
+        }
+
+        private void addSymbol(string symbol)
+        {
+            if (previousListViews.Select(x => x.Items.Cast<ListViewItem>().Last()).Any(x => x.Text.Equals(symbol)))
+            {
+                return;
+            }
+
+            currentListView.Items.Add(symbol);
+
+            previousListViews.Add(currentListView);
+
+            if (currentListView == listViewItem1)
+            {
+                currentListView = listViewItem2;
+                return;
+            }
+
+            if (currentListView == listViewItem2)
+            {
+                currentListView = listViewItem3;
+                return;
+            }
+
+            // It was not item1 or item2 so it must be item3
+            checkLine();
+
+            previousListViews.Clear();
+            currentListView = listViewItem1;
+        }
+
+        private void checkLine()
+        {
+            var currentLine = (new ListView[] { listViewItem1, listViewItem2, listViewItem3 }).Select(
+                x => x.Items.Cast<ListViewItem>().Last().Text
+                ).ToList();
+
+            var currentResultListViewIndex = 0;
+
+            var correctSymbolsToHandle = correctLine.ToList();
+            var currentSymbolsToHandle = currentLine.ToList();
+
+            var correctLineSelection = correctLine.Select((value, i) => new { i, value });
+            // Check for full on correct positions
+            foreach (var item in correctLineSelection)
+            {
+                if (item.value == currentLine[item.i])
+                {
+                    resultListViews[currentResultListViewIndex].Items.Add("X");
+                    currentResultListViewIndex++;
+                    correctSymbolsToHandle.Remove(item.value);
+                    currentSymbolsToHandle.Remove(item.value);
+                }
+            }
+
+            // Check for correct symbol but wrong position
+            foreach (var item in correctLineSelection.Where(x => correctSymbolsToHandle.Any(y => y == x.value)))
+            {
+                if (currentLine.ToList().Where(x => currentSymbolsToHandle.Any(y => y == x)).Any(x => x == item.value))
+                {
+                    resultListViews[currentResultListViewIndex].Items.Add("O");
+                    currentResultListViewIndex++;
+                    correctSymbolsToHandle.Remove(item.value);
+                    currentSymbolsToHandle.Remove(item.value);
+                }
+            }
+
+            // Check for correct character 
+            foreach (var item in correctLineSelection.Where(x => correctSymbolsToHandle.Any(y => y == x.value)))
+            {
+                if (currentLine.ToList().Where(x => currentSymbolsToHandle.Any(y => y == x)).Any(x => x[0] == item.value[0]))
+                {
+                    resultListViews[currentResultListViewIndex].Items.Add("C");
+                    currentResultListViewIndex++;
+                    correctSymbolsToHandle.Remove(item.value);
+                    currentSymbolsToHandle.Remove(item.value);
+                }
+            }
+
+            // Check for correct number
+            foreach (var item in correctLineSelection.Where(x => correctSymbolsToHandle.Any(y => y == x.value)))
+            {
+                if (currentLine.ToList().Where(x => currentSymbolsToHandle.Any(y => y == x)).Any(x => x[1] == item.value[1]))
+                {
+                    resultListViews[currentResultListViewIndex].Items.Add("N");
+                    currentResultListViewIndex++;
+                    correctSymbolsToHandle.Remove(item.value);
+                    currentSymbolsToHandle.Remove(item.value);
+                }
+            }
+
+            for (; currentResultListViewIndex < 3; currentResultListViewIndex++)
+            {
+                resultListViews[currentResultListViewIndex].Items.Add(" ");
+            }
+        }
+    }
+}
